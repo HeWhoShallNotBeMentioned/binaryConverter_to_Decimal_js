1d13e4a0294c09a67302c782cda431f631ff4816
Tim Kellogg & Chris Underwood <student@epicodus.com>
2015-09-10 16:52:04 -0700

    added styling and fixed the customBase function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var base = function(binary) {
 
 	var binaryArray = binary.split('').reverse();
-	var sum = 0; 
+	var sum = 0;
 
 	binaryArray.forEach(function(currentValue, index){
 		if (currentValue == 1) {
@@ -10,3 +10,34 @@ var base = function(binary) {
 	});
 	return sum;
 };
+
+var customBase = function(binary, place) {
+	var number = base(binary);
+	return Math.pow(number, place);
+};
+
+$(document).ready(function() {
+  $("form#bases").submit(function(event) {
+    var binary = $("input#base").val();
+		var baseValue = $("input#custom_base").val();
+    var result = customBase(binary, baseValue);
+
+
+  $(".result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+
+
+	// Background colors
+	var colors = ['lightred', 'red', 'darkred'];
+
+
+	setInterval(function() {
+		var backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+		$(document.body).css('background-color', backgroundColor);
+
+	}, 10000);
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -14,3 +14,14 @@ describe("base", function() {
     expect(base("01000001010000100110101110111101100")).to.equal(8758975980);
   })
 });
+
+
+// customBase
+describe("customBase", function() {
+  it('returns 0 for a string of "0"', function() {
+    expect(customBase("11", 3)).to.equal(27);
+  });
+  it('return "10001" ', function() {
+    expect(customBase("10001", 3)).to.equal(4913);
+  });
+});
