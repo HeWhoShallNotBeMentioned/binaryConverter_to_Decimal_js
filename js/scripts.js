a6abc0b01876f198b3ea4f9056a7b7341f96e73a
Tim Kellogg & Chris Underwood <student@epicodus.com>
2015-09-10 14:04:12 -0700

    add initial files and pass the first spec

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var base = function(binary) {
+
+	var binaryArray = binary.split('');
+	var sum = 0; //value to store the result of looking up the value of binaryArray positions
+
+	// go through the binaryArray, find the index of the number searched
+	// raise it to the index's power
+	// add to sum
+
+	// return sum
+	return 0;
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("base", function() {
+  it('returns 0 for a string of "0"', function() {
+    expect(base("0")).to.equal(0);
+  });
+});
